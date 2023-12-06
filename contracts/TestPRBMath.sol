948a0b00f7e14c9292d011e329c0cb9fe0641040
Andrews Nobre <andrewsnobre@gmail.com>
2023-12-05 23:23:05 -0300

    ok

--- contracts/TestPRBMath.sol
+++ contracts/TestPRBMath.sol
@@ -7,7 +7,7 @@ import "hardhat/console.sol";
 
 contract TestPRBMath {
     function testPrecision() public view returns (bool) {
-        uint256 base = 5e18; // 50 in fixed-point notation
+        uint256 base = 5e18; // 5 in fixed-point notation
         uint256 exponent = 3e18; // 3 in fixed-point notation
 
         uint256 resultPRBMath = PRBMathUD60x18.pow(base, exponent);
