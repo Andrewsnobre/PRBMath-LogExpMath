9612d214dc99cd188d6644883dee193fcb4dcb7b
Andrews Nobre <andrewsnobre@gmail.com>
2023-12-05 23:01:45 -0300

    English

--- contracts/TestPRBMath.sol
+++ contracts/TestPRBMath.sol
@@ -7,24 +7,24 @@ import "hardhat/console.sol";
 
 contract TestPRBMath {
     function testPrecision() public view returns (bool) {
-        uint256 base = 50e18; // 2 em notação de ponto fixo
-        uint256 exponent = 3e18; // 3 em notação de ponto fixo
+        uint256 base = 50e18; // 2 in fixed-point notation
+        uint256 exponent = 3e18; // 3 in fixed-point notation
 
         uint256 resultPRBMath = PRBMathUD60x18.pow(base, exponent);
         uint256 resultLogExpMath = LogExpMath.pow(base, exponent);
-        uint256 resultHighPrecision = 125000e18; // Resultado esperado com alta precisão
+        uint256 resultHighPrecision = 125000e18; // Expected result with high precision
 
-        // Calcule a diferença absoluta e a porcentagem de diferença
+        // Calculate the absolute difference and percentage difference
         uint256 diff = _calculateDifference(resultPRBMath, resultHighPrecision);
         uint256 diffPercentage = _calculatePercentageDifference(diff, resultHighPrecision);
 
         uint256 diff2 = _calculateDifference(resultLogExpMath, resultHighPrecision);
         uint256 diff2Percentage = _calculatePercentageDifference(diff2, resultHighPrecision);
 
-        // Verifique se a diferença está dentro de um limite aceitável
-        uint256 acceptableErrorLimit = 1e16; // Defina seu limite aceitável
-        console.log("Diferenca PRBMath (absoluta, percentual):", diff, diffPercentage);
-        console.log("Diferenca LogExpMath (absoluta, percentual):", diff2, diff2Percentage);
+        // Check if the difference is within an acceptable limit
+        uint256 acceptableErrorLimit = 1e16; // Set your acceptable limit
+        console.log("PRBMath Difference (absolute, percentage):", diff, diffPercentage);
+        console.log("LogExpMath Difference (absolute, percentage):", diff2, diff2Percentage);
 
         return (diff < acceptableErrorLimit && diff2 < acceptableErrorLimit);
     }
@@ -34,6 +34,6 @@ contract TestPRBMath {
     }
 
     function _calculatePercentageDifference(uint256 diff, uint256 reference1) internal pure returns (uint256) {
-        return diff * 1e18 / reference1; // Resultado em notação de ponto fixo
+        return diff * 1e18 / reference1; // Result in fixed-point notation
     }
 }
--- test/testPRBMath.js
+++ test/testPRBMath.js
@@ -6,9 +6,6 @@ describe("TestPRBMath", function () {
     const TestPRBMath = await ethers.getContractFactory("TestPRBMath");
     const testPrbMath = await TestPRBMath.deploy();
 
-    // Não precisa mais desta linha:
-    // await testPrbMath.deployed();
-
     expect(await testPrbMath.testPrecision()).to.equal(true);
   });
 });
