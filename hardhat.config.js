b76e786ecee30a735bc2bf7ae35c5f74b1fe1aae
Andrews Nobre <andrewsnobre@gmail.com>
2023-12-07 09:05:45 -0300

    size script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
   gasReporter: {
     enabled: true,
+  },
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: true,
+    unit: "B",
   },
 };
