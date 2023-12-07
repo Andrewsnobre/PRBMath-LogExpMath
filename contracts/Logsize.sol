67252a106a1d6d2a0ef320f6740d14d470978a16
Andrews Nobre <andrewsnobre@gmail.com>
2023-12-07 09:09:10 -0300

    ok

--- contracts/Logsize.sol
+++ contracts/Logsize.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.4;
 
 import "./LogExpMath.sol"; 
 
-contract Test {
+contract TestLog {
     function testPow(uint256 base, uint256 exponent) public pure returns (uint256) {
         return LogExpMath.pow(base, exponent);
     }
